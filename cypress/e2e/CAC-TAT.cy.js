9404abe153e144989c1744c667dfc1517dd39eb4
isaacfmartins <isaacfmartins@hotmail.com>
2025-04-15 20:58:30 -0300

    Create cypress project

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,292 @@
+const { faker, base} = require('@faker-js/faker')
+
+const user = {}
+
+beforeEach(() => {
+
+  cy.visit('./src/index.html')
+  user.primeiroNome = faker.person.firstName()
+  user.sobreNome = faker.person.lastName()
+  user.email = faker.internet.email()
+  user.descriacaoTexto = faker.lorem.sentence()
+})
+
+
+describe('Central de Atendimento ao Cliente TAT', () => {
+
+
+
+  it('verifica o título da aplicação', () => {
+
+    cy.title().should('eq', 'Central de Atendimento ao Cliente TAT');
+    cy.get('#title').should('have.text', 'CAC TAT');
+
+
+  })
+
+  it('Preenchendo todos os campos obrigatórios', () => {
+    
+    const longText = user.descriacaoTexto.repeat(5)
+
+    cy.get('#firstName').as('firstName')
+    cy.get('#lastName').as('lastName')
+    cy.get('#email').as('email')
+    cy.get('#open-text-area').as('open-text-area')
+    cy.get('@firstName').type(user.primeiroNome)
+    cy.get('@lastName').type(user.sobreNome)
+    cy.get('@email').type(user.email)
+    cy.get('@open-text-area').type(longText, { delay: 0 })
+
+    cy.get('button[type="submit"]').as('button')
+    cy.get('@button').click()
+
+    cy.get('span[class="success"] strong').as('mensagemSuccess')
+    cy.get('@mensagemSuccess').should('be.visible')
+    cy.get('@mensagemSuccess').should('have.text', 'Mensagem enviada com sucesso.');
+
+  })
+
+  it('Preenchendo todos os campos obrigatórios com delay na mensagem', () => {
+
+    cy.get('#firstName').as('firstName')
+    cy.get('#lastName').as('lastName')
+    cy.get('#email').as('email')
+    cy.get('#open-text-area').as('open-text-area')
+    cy.get('@firstName').type(user.primeiroNome)
+    cy.get('@lastName').type(user.sobreNome)
+    cy.get('@email').type(user.email)
+    cy.get('@open-text-area').type(user.descriacaoTexto)
+    cy.get('button[type="submit"]').as('button')
+    cy.get('span[class="success"] strong').as('mensagemSuccess')
+    cy.get('@button').click()
+
+    cy.get('@mensagemSuccess').should('be.visible')
+    cy.get('@mensagemSuccess').should('have.text', 'Mensagem enviada com sucesso.', { delay: 0 });
+
+  })
+
+  it('Preenchendo formulário com email invalido', () => {
+
+    cy.get('#firstName').as('firstName')
+    cy.get('#lastName').as('lastName')
+    cy.get('#email').as('email')
+    cy.get('#open-text-area').as('open-text-area')
+    cy.get('@firstName').type(user.primeiroNome)
+    cy.get('@lastName').type(user.sobreNome)
+    cy.get('@email').type(user.primeiroNome)
+    cy.get('@open-text-area').type(user.descriacaoTexto)
+    cy.get('button[type="submit"]').as('button')
+    cy.get('span[class="error"] strong').as('mensagemError')
+    cy.get('@button').click()
+
+    cy.get('@mensagemError').should('be.visible')
+    cy.get('@mensagemError').should('have.text', 'Valide os campos obrigatórios!', { delay: 0 });
+
+  })
+
+  it('verifica se campo telefone aceita apenas número', () => {
+
+    cy.get('#firstName').as('firstName')
+    cy.get('#lastName').as('lastName')
+    cy.get('#email').as('email')
+    cy.get('#open-text-area').as('open-text-area')
+    cy.get('@firstName').type(user.primeiroNome)
+    cy.get('@lastName').type(user.sobreNome)
+    cy.get('@email').type(user.email)
+    cy.get('@open-text-area').type(user.descriacaoTexto)
+    cy.get('button[type="submit"]').as('button')
+    cy.get('span[class="error"] strong').as('mensagemError')
+    cy.get('#phone').as('phone')
+    cy.get('@phone').type('abc')
+    cy.get('@phone').should('not.have.value', /^[A-Za-z]+$/).clear();
+    cy.get('@button').click()
+
+    cy.get('@mensagemError').should('exist')
+    cy.get('@mensagemError').should('have.text', 'Valide os campos obrigatórios!', { delay: 0 });
+
+  })
+
+  it('verifica se telefone é obrigatório', () => {
+
+    cy.get('#firstName').as('firstName')
+    cy.get('#lastName').as('lastName')
+    cy.get('#email').as('email')
+    cy.get('#open-text-area').as('open-text-area')
+    cy.get('@firstName').type(user.primeiroNome)
+    cy.get('@lastName').type(user.sobreNome)
+    cy.get('@email').type(user.email)
+    cy.get('@open-text-area').type(user.descriacaoTexto)
+    cy.get('#phone').as('phone')
+    cy.get('#phone-checkbox').as('phone-checkbox')
+    cy.get('@phone-checkbox').check()
+    cy.get('@phone').type(' ')
+    cy.get('@phone').should('not.have.value', /^[A-Za-z]+$/).clear();
+    cy.get('button[type="submit"]').as('button')
+    cy.get('@button').click()
+    
+    cy.get('span[class="error"] strong').as('mensagemError')
+    cy.get('@mensagemError').should('be.visible')
+    cy.get('@mensagemError').should('have.text', 'Valide os campos obrigatórios!', { delay: 0 });
+
+  })
+  it('verifica se preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+
+    cy.get('#firstName').as('firstName')
+    cy.get('#lastName').as('lastName')
+    cy.get('#email').as('email')
+    cy.get('#open-text-area').as('open-text-area')
+    cy.get('@firstName').type(user.primeiroNome)
+    cy.get('@lastName').type(user.sobreNome)
+    cy.get('@email').type(user.email)
+    cy.get('@open-text-area').type(user.descriacaoTexto)
+    cy.get('@firstName').should('have.value', (user.primeiroNome)).clear()
+    .should('have.value', (''))
+    cy.get('@lastName').should('have.value', (user.sobreNome)).clear()
+    .should('have.value', (''))
+    cy.get('@email').should('have.value', (user.email)).clear()
+    .should('have.value', (''))
+    cy.get('@open-text-area').should('have.value', (user.descriacaoTexto)).clear()
+    .should('have.value', ('')) 
+    cy.get('#phone').as('phone')
+    cy.get('#phone-checkbox').as('phone-checkbox')
+    cy.get('@phone-checkbox').check()
+    cy.get('@phone').type(' ')
+    cy.get('@phone').should('not.have.value', /^[A-Za-z]+$/).clear()
+    .should('have.value', '');
+    cy.get('button[type="submit"]').as('button')
+    cy.get('@button').click()
+    
+    cy.get('span[class="error"] strong').as('mensagemError')
+    cy.get('@mensagemError').should('be.visible')
+    cy.get('@mensagemError').should('have.text', 'Valide os campos obrigatórios!', { delay: 0 });
+
+  })
+
+  it('verifica se exibe mensagem de erro ao enviar formulário com campos obrigatórios vazios', () => {
+    cy.get('button[type="submit"]').as('button').click()
+    
+    cy.get('span[class="error"] strong').as('mensagemError')
+    .should('be.visible')
+    .should('have.text', 'Valide os campos obrigatórios!', { delay: 0 });
+
+  })
+
+  it('verifica se preenche usando Comandos Customizados', () => {
+    cy.fillMandatoryFieldAndSubmit(user)
+    cy.get('span[class="success"] strong').as('mensagemSuccess')
+    .should('be.visible')
+    .should('have.text', 'Mensagem enviada com sucesso.');
+
+  })
+
+  it('verifica se preenche usando Contains', () => {
+    cy.fillMandatoryFieldAndSubmitStandard()
+    cy.contains('sucesso.')
+
+  })
+
+  it('Seleciona produto YouTube', () => { 
+    
+  
+    cy.get('#firstName').type(user.primeiroNome)
+    cy.get('#lastName').type(user.sobreNome)
+    cy.get('#email').type(user.email)
+    cy.get('#product').select('YouTube')
+    cy.get('#product').should('have.value', 'youtube')
+    cy.get('#open-text-area').type(user.descriacaoTexto)
+
+    cy.get('button[type="submit"]').click()
+  })
+  
+  it('Seleciona produto Mentoria', () => {
+    cy.get('#firstName').type(user.primeiroNome)
+    cy.get('#lastName').type(user.sobreNome)
+    cy.get('#email').type(user.email)
+    cy.get('#product').select('mentoria')
+    cy.get('#product').should('have.value', 'mentoria')
+    cy.get('#product').should('have.value', 'mentoria')
+    cy.get('#open-text-area').type(user.descriacaoTexto)
+
+    cy.get('button[type="submit"]').click()  
+  })
+
+  it('Seleciona produto Blog', () => {
+    cy.get('#firstName').type(user.primeiroNome)
+    cy.get('#lastName').type(user.sobreNome)
+    cy.get('#email').type(user.email)    
+    cy.get('#product').select(1)
+    cy.get('#product').should('have.value', 'blog')
+    cy.get('#open-text-area').type(user.descriacaoTexto)
+
+    cy.get('button[type="submit"]').click()
+  })
+
+  it('Seeciona produto Aleatório', () => {
+    cy.fillMandatoryFieldAndSubmit(user)
+  })
+
+  it('Verifica se o radiobutton "Feedback" foi marcado', () => {
+    cy.get('input[type=radio][value="feedback"]').check()
+    .should('be.checked')
+  })
+
+  it('Verifica se marca cada tipo de atendimento', () => {
+    cy.get('input[type=radio]').each(($elementos) => {
+        cy.wrap($elementos)
+        .check()
+        .should('be.checked')
+    })
+
+  })
+
+  it('Verifica se o checkboxes foram marcados', () => {
+    cy.get('input[type=checkbox]')
+    .check()
+    .should('be.checked')
+    .last()
+    .uncheck()
+    .should('be.not.checked')
+  })
+
+  it('seleciona um arquivo da pasta fixtures', () => {
+    cy.get('input[type="file"]').selectFile('cypress/fixtures/example.json')
+    .then(input => {
+      expect(input[0].files[0].name).to.eq('example.json')
+    })
+  })
+
+  it('seleciona um arquivo da simulando um drag-and-drop', () => {
+    cy.get('input[type="file"]').selectFile('cypress/fixtures/example.json', {action: 'drag-drop'})
+    .then(input => {
+      expect(input[0].files[0].name).to.eq('example.json')
+    })
+  })
+
+  it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture('example.json', null).as('exampleFile'); 
+
+    cy.get('input[type="file"]').selectFile('@exampleFile')
+    .then(input => {
+      expect(input[0].files[0].name).to.eq('example.json')
+    })
+  
+  })
+
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique',()=>{
+
+    cy.get('#privacy a').should('have.attr', 'href', 'privacy.html').and('have.attr', 'target', '_blank')
+
+  })
+
+  it('acessa a página da política de privacidade removendo o target e então clicando no link',()=>{
+
+    cy.get('#privacy a').invoke('removeAttr', 'target').click()
+    cy.url().should('include', 'privacy.html')
+
+  })
+
+ 
+
+
+  
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,62 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+Cypress.Commands.add('fillMandatoryFieldAndSubmit',  user => { 
+    
+    cy.get('#firstName').type(user.primeiroNome)
+    cy.get('#lastName').type(user.sobreNome)
+    cy.get('#email').type(user.email) 
+    cy.pickRandomOption()
+    cy.get('#open-text-area').type(user.descriacaoTexto)
+
+    cy.get('button[type="submit"]').as('button').click()
+    
+ })
+
+
+ Cypress.Commands.add('pickRandomOption', () => {
+    cy.get('select')
+  .find('option') // Encontra todas as opções
+  .then(($options) => {
+    const randomIndex = Math.floor(Math.random() * $options.length); // Gera um índice aleatório
+    const randomValue = $options[randomIndex].value; // Obtém o valor da opção aleatória
+    
+    cy.get('select').select(randomValue); // Seleciona a opção aleatória
+  });   
+
+ })
+
+Cypress.Commands.add('fillMandatoryFieldAndSubmitStandard',  (user = {
+    primeiroNome: 'John',
+    sobreNome: 'Brown',
+    email: '2Ht4x@example.com',
+    descriacaoTexto: 'Teste'
+}) => {  
+    cy.get('#firstName').type(user.primeiroNome)
+    cy.get('#lastName').type(user.sobreNome)
+    cy.get('#email').type(user.email)
+    cy.get('#open-text-area').type(user.descriacaoTexto)   
+    cy.get('button[type="submit"]').as('button').click()
+    
+ })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
